bfb40e8822401b0dc09b9f474e8f69227a5bc659
JBAnovling <jba.github@gmail.com>
2014-03-06 17:24:29 +0100

    mindre fejl :-) slugs = labels

--- SQL/function_getAndInsertAllLabels.sql
+++ SQL/function_getAndInsertAllLabels.sql
@@ -3,20 +3,20 @@ DROP FUNCTION IF EXISTS getAndInsertAllLabels() CASCADE;
 CREATE OR REPLACE FUNCTION getAndInsertAllLabels() RETURNS VARCHAR AS $$
 << outerblock >>
 DECLARE
-  slugs		text[];
+  labels	text[];
   count		integer default 0;
   v_found	VARCHAR default 'N';
 BEGIN
 
-  slugs := (SELECT xpath('//ProgramSerie/Labels/string/text()', downloadedXML.documentContent) FROM downloadedXML);
+  labels := (SELECT xpath('//ProgramSerie/Labels/string/text()', downloadedXML.documentContent) FROM downloadedXML);
 
-  FOR i IN array_lower(slugs, 1) .. array_upper(slugs, 1)
+  FOR i IN array_lower(labels, 1) .. array_upper(labels, 1)
   LOOP
     v_found := NULL;
-    SELECT labelNo INTO v_found FROM label WHERE label.labelTxt = slugs[i];
+    SELECT labelNo INTO v_found FROM label WHERE label.labelTxt = labels[i];
     IF v_found IS NULL THEN
       count := count + 1;
-      INSERT INTO label(labelTxt) VALUES(slugs[i]);
+      INSERT INTO label(labelTxt) VALUES(labels[i]);
     END IF;
   END LOOP;
 
