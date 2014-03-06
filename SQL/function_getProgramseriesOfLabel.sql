b43ff58e5762da67a39398b035db83a360b3c5f9
Elkido <stunplugged@gmail.com>
2014-03-06 20:25:07 +0100

    sql fct programseries of label

---
+++ SQL/function_getProgramseriesOfLabel.sql
@@ -0,0 +1,38 @@
+﻿DROP FUNCTION IF EXISTS fct_programseriesOfLabel(character(50)) CASCADE;
+
+CREATE FUNCTION fct_programseriesOfLabel(labelName character(50) ) returns SETOF xml --programserie
+AS $$
+DECLARE
+
+BEGIN
+	--RETURN QUERY SELECT *
+	
+	RETURN QUERY SELECT XMLElement( name "programseries",
+		XMLAgg( XMLElement( name "programserie",
+				XMLElement( name "slug", ps.slug),
+				XMLElement( name "titel", ps.title),
+				XMLElement( name "description", ps.description),
+				XMLElement( name "newestvideoid", ps.newestvideoid),
+				XMLElement( name "newestvideopublishtime", ps.newestvideopublishtime)
+			)	
+		)
+		)
+		
+	FROM programserie AS ps
+	NATURAL JOIN
+	(
+		SELECT sla.programserieno
+		FROM sluglabel AS sla
+		WHERE  sla.labelno = (
+			SELECT la.labelno
+			FROM label as la
+			WHERE labeltxt = labelName
+		)
+	)selNo;
+	
+END;
+$$
+LANGUAGE plpgsql;
+
+
+
